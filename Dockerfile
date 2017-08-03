84507e7cfdf361e67c7e37c6d9a75af578240044
Ahmed-Ramadan <Ahmed-Ramadan>
2017-08-03 16:51:49 +0200

    front

--- Dockerfile
+++ Dockerfile
@@ -1,24 +1,12 @@
-#FROM microsoft/aspnetcore:1.1
-#ARG source
-#WORKDIR /app
-#EXPOSE 80
-#COPY ${source:-obj/Docker/publish} .
-#ENTRYPOINT ["dotnet", "Talabat.FrontEndDemo.Web.dll"]
 
-
-FROM microsoft/aspnetcore-build:1.1 AS build-env
+FROM microsoft/dotnet:1.1.1-runtime
+#FROM microsoft/aspnetcore-build:1.1 AS build-env
 WORKDIR /app
 
-# copy csproj and restore as distinct layers
-COPY *.csproj ./
-RUN dotnet restore
 
-# copy everything else and build
+#RUN dotnet publish -c Release -o out
 COPY . ./
-RUN dotnet publish -c Release -o out
-
 # build runtime image
-FROM microsoft/aspnetcore:1.1
-WORKDIR /app
-COPY --from=build-env /app/out .
+
+
 ENTRYPOINT ["dotnet", "Talabat.FrontEndDemo.Web.dll"]
--- wwwroot/Scripts/app.js
+++ wwwroot/Scripts/app.js
@@ -2,32 +2,61 @@
 
 angular.module("myModule").constant("rootUrl", {
     order: "http://localhost:4121/talabat/order",
-    restaurant: "http://localhost:4140/talabat/restaurant"
+    restaurant: "http://localhost:4140/talabat/restaurant",
+    orderToken: "http://localhost:4121/api/token",
+    restaurantToken:"http://localhost:4140/api/token"
 });
 
 angular.module("myModule")
     .controller("orderController", ["$scope", "$http", "rootUrl", function ($scope, $http, rootUrl) {
-        //$scope.pageName = "Talabat First Page";
-        // Id 
-        // OrderDetails 
-        // Quantity 
-        // Status 
-        // ResturantId 
+
+        $scope.list = [
+             {
+                key: 1,
+                value: "KFC"
+            },
+             {
+                key: 2,
+                value: "SFC Plus"
+             },
+             {
+                 key: 3,
+                 value: "Zaatar W Aktar"
+             },
+             {
+                 key: 4,
+                 value: "Burger King"
+             }
+        ]
+
         $scope.submit = function () {
             $scope.order = {
-                ResturantId: $scope.restaurant,
-                OrderDetails: $scope.item,
+                ResturantId: $scope.restaurant.key,
+                Item: $scope.item,
+                ResturantName: $scope.restaurant.value,
                 Quantity: $scope.quantity
             };
             if (validate()) {
-                $http.post(rootUrl.order + "/create", $scope.order).then(
-                    function (obj) {
-                        console.log(obj);
-                    },
-                    function (error) {
+
+                $http.post(rootUrl.orderToken, { name: "admin", password: "admin" })
+                    .then(function (obj) {
+                        let auth = obj.data;
+                        let token = 'bearer ' + auth.token;
+                        $http.post(rootUrl.order + "/create", $scope.order,
+                            {
+                                headers: {
+                                    'Authorization': token
+                                }
+                            })
+                            .then(function (obj) {
+                                console.log(obj);
+                            },
+                            function (error) {
+                                console.log(error);
+                            });
+                    }, function (error) {
                         console.log(error);
-                    }
-                );
+                    });
             }
         };
 
@@ -46,12 +75,24 @@ angular.module("myModule")
 angular.module("myModule")
     .controller("restaurantController", ["$scope", "$http", "rootUrl", "$filter", function ($scope, $http, rootUrl, $filter) {
         let getRestaurat = function () {
-            $http.get(rootUrl.restaurant)
+
+            $http.post(rootUrl.restaurantToken, { name: "admin", password: "admin" })
                 .then(function (obj) {
-                    $scope.restOrder = obj.data.filter(function (el) {
-                        return el.status == 0;
-                    });
-                    console.log(obj.data);
+                    let auth = obj.data;
+                    let token = 'bearer ' + auth.token;
+                    $http.get(rootUrl.restaurant,
+                        {
+                            headers: {
+                                'Authorization': token
+                            }
+                        }).then(function (obj) {
+                            $scope.restOrder = obj.data.filter(function (el) {
+                                return el.status === 0;
+                            });
+                            console.log(obj.data);
+                        }, function (error) {
+                            console.log(error);
+                        });
                 }, function (error) {
                     console.log(error);
                 });
@@ -63,21 +104,49 @@ angular.module("myModule")
             //accepted order
             resturant.status = 1;
             let val = resturant;
-            $http.post(rootUrl, resturant).then(function (obj) {
-                console.log(obj.data);
-            }, function (error) {
-                console.log(error);
+            $http.post(rootUrl.restaurantToken, { name: "admin", password: "admin" })
+                .then(function (obj) {
+                    let auth = obj.data;
+                    let token = 'bearer ' + auth.token;
+                    $http.put(rootUrl.restaurant, resturant, {
+                        headers: {
+                            'Authorization': token
+                        }
+                    }).then(function (obj)
+                    {
+                        console.log(obj.data);
+                        getRestaurat();
+                    }, function (error) {
+                        console.log(error);
+                    });
+                }, function (error) {
+                    console.log(error);
                 });
         };
         $scope.reject = function (resturant) {
             //rejected oreder
             resturant.status = 2;
             let val = resturant;
-            $http.delete(rootUrl.restaurant + "/" + resturant.orderId).then(function (obj) {
-                console.log(obj.data);
-            }, function (error) {
-                console.log(error);
+            $http.post(rootUrl.restaurantToken, { name: "admin", password: "admin" })
+                .then(function (obj) {
+                    let auth = obj.data;
+                    let token = 'bearer ' + auth.token;
+                    $http.delete(rootUrl.restaurant + "/" + resturant.orderId,
+                        {
+                            headers: {
+                                'Authorization': token
+                            }
+                        }).then(function (obj)
+                        {
+                            console.log(obj.data);
+                            getRestaurat();
+                        }, function (error)
+                        {
+                            console.log(error);
+                        });
+                }, function (error) {
+                    console.log(error);
                 });
-        }; 
+        };
 
     }]);
